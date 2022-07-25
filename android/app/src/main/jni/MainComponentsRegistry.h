7e4cec24f3c146cc34ee09f5c4a9a908c115ceae
h4mxa <muhammad.hamza@arkhitech.com>
2022-07-25 10:29:53 +0500

    first commit

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/embeddedprinter/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/embeddeprinter/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
---
+++ ios/Pods/Target Support Files/Pods-embeddePrinter-embeddePrinterTests/Pods-embeddePrinter-embeddePrinterTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_embeddePrinter_embeddePrinterTests : NSObject
+@end
+@implementation PodsDummy_Pods_embeddePrinter_embeddePrinterTests
+@end
