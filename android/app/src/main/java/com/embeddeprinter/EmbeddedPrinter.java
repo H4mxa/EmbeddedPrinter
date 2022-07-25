876d32665fa2073314eb999bc3719c6765a574b6
h4mxa <muhammad.hamza@arkhitech.com>
2022-07-25 10:45:43 +0500

    configure native modules

---
+++ android/app/src/main/java/com/embeddeprinter/EmbeddedPrinter.java
@@ -0,0 +1,12 @@
+package com.embeddeprinter;
+
+import android.util.Log;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+
+public class EmbeddedPrinter {
+
+    public EmbeddedPrinter(ReactApplicationContext reactContext) {
+
+    }
+}
